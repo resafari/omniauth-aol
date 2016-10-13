fc3ae55f590ef1ee3b70eb558c53c93a3c88a71e
Jon Christopher <joncirca@icloud.com>
2016-10-12 23:18:41 -0700

    Update aol.rb

--- lib/omniauth/strategies/aol.rb
+++ lib/omniauth/strategies/aol.rb
@@ -12,29 +12,12 @@ module OmniAuth
         :token_url => 'https://api.screenname.aol.com/auth/access_token',
       }
 
-#       uid { "placeholder" }
-
-#       info do
-#         {
-#             :email => "placeholder"
-#         }
-#       end
-
-#       extra do
-#         {
-#             'raw_info' => raw_info
-#         }
-#       end
-
       def build_access_token
         verifier = request.params['code']
         redirect_uri = URI.parse(callback_url).tap { |uri| uri.query = nil }.to_s
         client.auth_code.get_token(verifier, {:redirect_uri => redirect_uri}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
       end
       
-#       def raw_info
-#         @raw_info ||= Hash.new
-#       end
     end
   end
 end
