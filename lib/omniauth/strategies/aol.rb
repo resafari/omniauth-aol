dfa40ca0524a5bbc3fb89d41ce7a524eeea72d1b
Jon Christopher <joncirca@icloud.com>
2016-10-12 23:05:21 -0700

    Update aol.rb

--- lib/omniauth/strategies/aol.rb
+++ lib/omniauth/strategies/aol.rb
@@ -1,6 +1,5 @@
 require 'omniauth/strategies/oauth2'
-require 'openssl'
-require 'rack/utils'
+require 'uri'
 
 module OmniAuth
   module Strategies
@@ -30,6 +29,13 @@ module OmniAuth
         }
       end
 
+      def build_access_token
+        puts "*"*100
+        verifier = request.params['code']
+        redirect_uri = URI.parse(callback_url).tap { |uri| uri.query = nil }.to_s
+        client.auth_code.get_token(verifier, {:redirect_uri => redirect_uri}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+      end
+      
       def raw_info
         @raw_info ||= access_token.get('/auth/getUserDataInternal?attribute=email&f=json').parsed
       end
